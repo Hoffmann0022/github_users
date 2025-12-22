c63dc000bd1c42b5c37b9ea34736b1ec1605b940
Hoffmann0022 <eduardohoffmann.sg022@academico.iful.edu.br>
2025-12-22 17:37:40 -0300

    test: Removing the workaround for error 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Login } from "./pages/login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { User } from "./pages/user";
 import { Repos } from "./pages/repos";
 import { LoadingContextProvider } from "./contexts/LoadingContext";
@@ -8,13 +8,13 @@ function App() {
 
   return (
     <LoadingContextProvider>
-      <BrowserRouter basename="/github_users">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/users/:userName" element={<User />} />
           <Route path="/repos/:userName/:reposName" element={<Repos />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LoadingContextProvider>
   )
 }
